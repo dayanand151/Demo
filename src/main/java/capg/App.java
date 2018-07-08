1fe676f933a0f8c31897eca752f09073319c1b2c
Astitva <dayanand151@gmail.com>
2018-07-08 18:05:21 +0530

    updated

---
+++ src/main/java/capg/App.java
@@ -0,0 +1,14 @@
+package capg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	System.out.println("My Nmae is khan");
+    }
+}
